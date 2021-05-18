3d405c3645c507da30020e874a39d47a4690d2b9
JMP <JMP@HOME-PC>
2021-05-18 17:45:16 +0900

    aa

--- p0518/src/Ex0518_05.java
+++ p0518/src/Ex0518_05.java
@@ -10,7 +10,6 @@ public class Ex0518_05 {
 		int num1 = scan.nextInt();
 		int num2 = scan.nextInt();
 		
-		System.out.println("데이터 : "+max(num1,num2));
 		
 		for (int i = 0; i < 10; i++) {
 			num1 += i;
@@ -20,16 +19,14 @@ public class Ex0518_05 {
 			num1 += i;
 		}
 		
-		max(num1,num2);
+		int result = add(num1,num2);
+		System.out.println(result);
 		
 	}//main메소드
-	static int max(int num1,int num2) {
-		int result = 0;
-		if (num1 < num2) {
-			result = num1;
-		}else {
-			result = num2;
-		}
+	//두수를 더해서 리턴해주는 메소드
+	static int add(int num1,int num2) {
+		int result = num1+num2;
 		return result;
 	}
+	
 }
---
+++ p0518/src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+	int a = 333;
+	int b = 45;
+	
+	Ex0518_05 e = new Ex0518_05();
+	int result = e.add(a, b);
+}
